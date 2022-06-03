caea55ad981007d2025dec360d0acf260600a6ff
RinalisDS <denispilyutin@gmail.com>
2022-06-03 16:17:31 +0300

    ограничение, чтобы не запрашивались данные с сервера, когда достигнут лимит

--- src/components/pages/DynamicPagination/DynamicPagination.tsx
+++ src/components/pages/DynamicPagination/DynamicPagination.tsx
@@ -5,42 +5,44 @@ import s from '../Photos/Photos.module.css'
 
 
 const DynamicPagination = () => {
-    const [photos, setPhotos] = useState<photosType[]>([])
-    const [currentPage, setCurrentPage] = useState<number>(1)
-    const [isFetching, setIsFetching] = useState<boolean>( true)
+  const [photos, setPhotos] = useState<photosType[]>([])
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [isFetching, setIsFetching] = useState<boolean>(true)
+  const [totalCount, setTotalCount] = useState(0)
 
-    useEffect(() => {
-        if (isFetching) {
-            console.log('fetching')
-            axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=10`)
-                .then((res: AxiosResponse<photosType[]>) => {
-                    setPhotos([...photos, ...res.data])
-                    setCurrentPage(prevState => prevState + 1)
-                })
-                .finally(() => setIsFetching(false))
-        }
-    }, [isFetching])
-
-    useEffect(() => {
-        document.addEventListener('scroll', scrollHandler)
-        return () => {
-            document.removeEventListener('scroll', scrollHandler)
-        }
-    }, [])
-    const scrollHandler = (event: any) => {
-        if (event.target.documentElement.scrollHeight - (event.target.documentElement.scrollTop + window.innerHeight) < 100) {
-            setIsFetching(true)
-        }
-
+  useEffect(() => {
+    if (isFetching) {
+      console.log('fetching')
+      axios.get(`https://jsonplaceholder.typicode.com/photos?_page=${currentPage}&_limit=10`)
+        .then((res: AxiosResponse<photosType[]>) => {
+          setPhotos([...photos, ...res.data])
+          setCurrentPage(prevState => prevState + 1)
+          setTotalCount(+res.headers['x-total-count'])
+        })
+        .finally(() => setIsFetching(false))
     }
-    return (
-        <div>
-            {photos.map(m => <div key={m.id} className={s.photos}>
-                <div>{m.id}. {m.title}</div>
-                <img src={m.thumbnailUrl} alt={'thumbnail'}/>
-            </div>)}
-        </div>
-    );
+  }, [isFetching])
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+    return () => {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
+  const scrollHandler = (event: any) => {
+    if (event.target.documentElement.scrollHeight - (event.target.documentElement.scrollTop + window.innerHeight) < 100 && photos.length < totalCount) {
+      setIsFetching(true)
+    }
+
+  }
+  return (
+    <div>
+      {photos.map(m => <div key={m.id} className={s.photos}>
+        <div>{m.id}. {m.title}</div>
+        <img src={m.thumbnailUrl} alt={'thumbnail'}/>
+      </div>)}
+    </div>
+  );
 };
 
 export default DynamicPagination;
